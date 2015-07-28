f12edad20ed049ed9c221028d87294e6e9ecee86
varinder <varinder.gupta@tcs.com>
2015-07-29 00:24:57 +0530

    AntIvy Project comming to GitHub

---
+++ src/com/vaannila/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.vaannila.helloworld;
+
+public class HelloWorld {
+	
+	
+	public static void main(String[] args){
+		HelloWorld instance = new HelloWorld();
+		instance.sayHello();
+	}
+
+	
+	public String sayHello() {
+		return "varinder";
+
+	}
+
+}
